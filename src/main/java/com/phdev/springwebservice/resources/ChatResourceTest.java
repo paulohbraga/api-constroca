9b1e5be59549f2f797b654e673b84aaa78d0c470
Paulo Henrique Braga <paulo@someletras.com.br>
2020-10-14 18:13:18 -0300

    add chatexists method in chatrepository

--- src/main/java/com/phdev/springwebservice/resources/ChatResourceTest.java
+++ src/main/java/com/phdev/springwebservice/resources/ChatResourceTest.java
@@ -54,9 +54,8 @@ public class ChatResourceTest {
 
 		if(exists) {
 			return ResponseEntity.ok(new Chat());
-		}else{
-			chat = chatRepository.save(chat);
 		}
+		chat = chatRepository.save(chat);
 
 		return ResponseEntity.ok().body(chat);
 
