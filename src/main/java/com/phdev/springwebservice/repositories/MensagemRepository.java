dc1bf9ef50903bee0b5cd06724c52bb1b31e5275
Paulo Henrique Braga <paulo@someletras.com.br>
2020-10-27 16:28:57 -0300

    testing

--- src/main/java/com/phdev/springwebservice/repositories/MensagemRepository.java
+++ src/main/java/com/phdev/springwebservice/repositories/MensagemRepository.java
@@ -14,7 +14,7 @@ import com.phdev.springwebservice.entities.Usuario;
 @Repository
 public interface MensagemRepository extends JpaRepository<Mensagem, Long> {
 	List<Mensagem> findByChatIdOrderByInstantAsc(Long id);
-    @Query(value = "SELECT sender FROM Mensagem m WHERE m.chat = 1", nativeQuery=true)
+    @Query(value = "SELECT sender FROM Mensagem m WHERE m.chat = 1 LIMIT 1", nativeQuery=true)
     String findSenderByChatId(Long id_chat);
 
 }
