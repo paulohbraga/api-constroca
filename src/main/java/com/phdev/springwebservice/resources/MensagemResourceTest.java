8e97330a4fcf9ef1c001d69742d75b65897d4349
Paulo Henrique Braga <paulo@someletras.com.br>
2020-10-29 01:09:48 -0300

    testing sender by id chat

--- src/main/java/com/phdev/springwebservice/resources/MensagemResourceTest.java
+++ src/main/java/com/phdev/springwebservice/resources/MensagemResourceTest.java
@@ -74,8 +74,8 @@ public class MensagemResourceTest {
 		
 		return chatRepository.findById(id_chat).map(chat -> {
 			mensagem.setInstant(Instant.now());
-			mensagem.setSender(a.toString());
-			mensagem.setReceiver(b.toString());
+			mensagem.setSender("50");
+			mensagem.setReceiver("93");
 			mensagem.setName_sender(userString);
 			mensagem.setName_receiver(userRString);
 			mensagem.setChat(chat);
