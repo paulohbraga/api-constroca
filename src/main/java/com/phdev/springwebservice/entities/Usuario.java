b9e426ee00680b9714630b0a9930e49742a0791c
Paulo Henrique Braga <paulo@someletras.com.br>
2020-11-21 23:19:57 -0300

    Add constraint for unique CPF/ID

--- src/main/java/com/phdev/springwebservice/entities/Usuario.java
+++ src/main/java/com/phdev/springwebservice/entities/Usuario.java
@@ -10,12 +10,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
-@Table(name = "tb_usuario")
+@Table(name = "tb_usuario",uniqueConstraints = @UniqueConstraint(columnNames = "cpf"))
 public class Usuario implements Serializable{
 
 	private static final long serialVersionUID = 1L;
